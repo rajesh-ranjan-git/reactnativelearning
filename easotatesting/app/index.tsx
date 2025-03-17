c8bb53c5f11a4fd720b285a63aa9d0fb7e8321ab
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-03-17 12:46:02 +0530

    Updates are working, checking for expo notifications now

--- easotatesting/app/index.tsx
+++ easotatesting/app/index.tsx
@@ -1,18 +1,11 @@
 import { Alert, Text, TouchableOpacity, View } from "react-native";
 import tw from "twrnc";
 import * as Updates from "expo-updates";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { checkForUpdates } from "./onLoadUtils";
 import { PermissionsAndroid } from "react-native";
-import messaging from "@react-native-firebase/messaging";
-
-// Register background handler
-messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-  console.log("Message handled in the background!", remoteMessage);
-});
 
 export default function Index() {
-  const [fcmTokenToShow, setFcmTokenToShow] = useState("");
   const { currentlyRunning, isUpdateAvailable, isUpdatePending } =
     Updates.useUpdates();
 
@@ -29,7 +22,6 @@ export default function Index() {
       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
         console.log("Notification permission granted");
         Alert.alert("Notification permission granted");
-        getToken();
       } else {
         console.log("Notification permission denied");
         Alert.alert("Notification permission denied");
@@ -39,23 +31,9 @@ export default function Index() {
     }
   };
 
-  const getToken = async () => {
-    const fcmToken = await messaging().getToken();
-    console.log("FCM token:", fcmToken);
-    setFcmTokenToShow(fcmToken);
-  };
-
   useEffect(() => {
     checkForUpdates();
     requestPermissionAndroid();
-  }, []);
-
-  useEffect(() => {
-    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
-      Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
-    });
-
-    return unsubscribe;
   }, []);
 
   return (
@@ -65,14 +43,12 @@ export default function Index() {
         This is a new text after updates.
       </Text>
       <Text style={tw`text-red-500 text-2xl`}>
-        Now I am testing notifications.
-      </Text>
-      <Text style={tw`text-red-500 text-2xl`}>Firebase setup done</Text>
-      <Text style={tw`text-red-500 text-2xl`}>
-        FCM Token : {fcmTokenToShow}
+        Now I am testing notifications again.
       </Text>
 
-      <Text style={tw`text-red-500 text-xl`}>This is after a new update.</Text>
+      <Text style={tw`text-red-500 text-xl`}>
+        This is after a another new update.
+      </Text>
 
       <Text style={tw`text-white text-xl`}>{runTypeMessage}</Text>
     </View>
